d0981ddfaf6ee382a5f54d0d9733a5be1c5da329
Isaac Chan <isaactinghong@gmail.com>
2020-06-11 22:56:05 +0800

    added tictactoe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from "react";
+import { render } from "react-dom";
+import { Client } from "boardgame.io/react";
+import TicTacToe from "./ttt/game";
+import Board from "./ttt/board";
+
+const App = Client({
+  game: TicTacToe,
+  board: Board,
+  // The number of players.
+  numPlayers: 2
+});
+
+// render(<App />, document.getElementById("root"));
 
 export default App;
---
+++ src/ttt/index.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from "react";
+import { render } from "react-dom";
+import { Client } from "boardgame.io/react";
+import TicTacToe from "./game";
+import Board from "./board";
+
+const App = Client({
+  game: TicTacToe,
+  board: Board,
+  // The number of players.
+  numPlayers: 2
+});
+
+render(<App />, document.getElementById("root"));
